68cbbfbf0764ce3bc49f3dda6a6cd917b06d4b15
Steve Goldhaber <stevenng@met.no>
2025-11-08 00:15:16 +0100

    Start on unit test for SDF reading

---
+++ test/unit_tests/test_sdf.py
@@ -0,0 +1,198 @@
+#! /usr/bin/env python3
+"""
+-----------------------------------------------------------------------
+ Description:  Contains unit tests for parsing Suite Definition Files (SDFs)
+               in scripts/parse_tools/xml_tools.py
+
+ Assumptions:
+
+ Command line arguments: none
+
+ Usage: python3 test_sdf.py         # run the unit tests
+-----------------------------------------------------------------------
+"""
+
+import filecmp
+import glob
+import logging
+import os
+import sys
+import unittest
+import xml.etree.ElementTree as ET
+
+_TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+_SCRIPTS_DIR = os.path.abspath(os.path.join(_TEST_DIR, os.pardir,
+                                            os.pardir, "scripts"))
+_SAMPLE_FILES_DIR = os.path.join(_TEST_DIR, "sample_suite_files")
+_PRE_TMP_DIR = os.path.join(_TEST_DIR, "tmp")
+_TMP_DIR = os.path.join(_PRE_TMP_DIR, "suite_files")
+
+if not os.path.exists(_SCRIPTS_DIR):
+    raise ImportError(f"Cannot find scripts directory, {_SCRIPTS_DIR}")
+
+sys.path.append(_SCRIPTS_DIR)
+
+# pylint: disable=wrong-import-position
+from parse_tools import init_log
+from parse_tools import read_xml_file, validate_xml_file, write_xml_file
+from parse_tools import find_schema_version, expand_nested_suites
+# pylint: enable=wrong-import-position
+
+class SDFParseTestCase(unittest.TestCase):
+
+    """Tests for `expand_nested_suites` and related functions."""
+
+    logger = None
+
+    @classmethod
+    def setUpClass(cls):
+        """Clean output directory (tmp) before running tests"""
+        # Does "tmp" directory exist?  If not then create it:
+        if not os.path.exists(_PRE_TMP_DIR):
+            os.makedirs(_PRE_TMP_DIR)
+        # end if
+
+        # We need a logger
+        cls.logger = init_log(cls.__name__, level=logging.WARNING)
+
+        #Does "tmp" directory exist?  If not then create it:
+        # Ensure the "tmp/suite_files" directory exists and is empty
+        if os.path.exists(_TMP_DIR):
+            # Clear out all files:
+            for fpath in glob.iglob(os.path.join(_TMP_DIR, '*.*')):
+                if os.path.exists(fpath):
+                    os.remove(fpath)
+                # End if
+            # End for
+        else:
+            os.makedirs(_TMP_DIR)
+        # end if
+
+        # Run inherited setup method:
+        super().setUpClass()
+
+    @classmethod
+    def get_logger(cls):
+        return cls.logger
+
+    @classmethod
+    def compare_text(cls, name, txt1, txt2, typ):
+        """Compare two XML text or tail items (which may be None).
+        Return None if items match, otherwise, return an error string"""
+        res = None
+        if txt1 and txt2:
+            if txt1 != txt2:
+                res = f"{name} {typ}, '{txt1}', does not match {typ}, '{txt2}'"
+            # end if
+        elif txt1:
+            res = f"{name} {typ} is missing from string2"
+        elif txt2:
+            res = f"{name} {typ} is missing from string1"
+        else:
+            res = None
+        # end if
+        return res
+
+    @classmethod
+    def xml_diff(cls, xt1, xt2):
+        """
+        Compares two xml etrees, xt1 and xt2
+        Return None if the trees match, otherwise, return a difference string
+        """
+
+        diffs = []
+        # First, compare the XML tags
+        if xt1.tag != xt2.tag:
+            diffs.append(f"Tags do not match: {xt1.tag} != {xt2.tag}")
+        else:
+            # Compare the attributes
+            for name, value in xt1.attrib.items():
+                xt2v = xt2.attrib.get(name)
+                if xt2v != value:
+                    diffs.append(f"Attributes for {name} do not match: {str(value)} != {str(xt2v)}")
+                # end if
+            # end for
+            for name in xt2.attrib.keys():
+                if name not in xt1.attrib:
+                    diffs.append(f"xt2 attribute, {name}, is missing in xt1")
+                # end if
+            # end for
+            # Compare the text bodies (if any)
+            tdiff = cls.compare_text(xt1.tag, xt1.text, xt2.text, "text")
+            if tdiff:
+                diffs.append(tdiff)
+            # end if
+            tdiff = cls.compare_text(xt1.tag, xt1.tail, xt2.tail, "tail")
+            if tdiff:
+                diffs.append(tdiff)
+            # end if
+            # Compare children
+            if len(xt1) != len(xt2):
+                diffs.append(f"Number of children length differs, {len(xt1)} != {len(xt2)}")
+            else:
+                for child1, child2 in zip(xt2, xt2):
+                    kid_diffs = cls.xml_diff(child1, child2)
+                    if kid_diffs:
+                        diffs.extend(kid_diffs)
+                    # end if
+                # end for
+            # end if
+        # end if
+        return diffs
+
+    @classmethod
+    def text_compare(cls, t1, t2):
+        """
+        Compare two text strings
+        :param t1: text one
+        :param t2: text two
+        :return:
+            True if a match
+        """
+        if not t1 and not t2:
+            return True
+        if t1 == '*' or t2 == '*':
+            return True
+        return (t1 or '').strip() == (t2 or '').strip()
+
+
+    def test_good_v1_sdf(self):
+        """Test that the parser recognizes a V1 SDF and parses it correctly
+        """
+        num_tests = 2
+        header = "Test of parsing of good V1 SDF"
+        for test_num in range(1,num_tests):
+            # Setup
+            testname = f"suite_good_v1_test{test_num:{0}{2}}"
+            source = os.path.join(_SAMPLE_FILES_DIR, f"{testname}.xml")
+            compare = os.path.join(_TMP_DIR, f"{testname}_out.xml")
+            logger = self.get_logger()
+            # Exercise
+            _, xml_root = read_xml_file(source, logger)
+            schema_version = find_schema_version(xml_root)
+            self.assertEqual(schema_version[0], 1)
+            self.assertEqual(schema_version[1], 0)
+            res = validate_xml_file(source, 'suite', schema_version, logger)
+            self.assertTrue(res)
+            write_xml_file(xml_root, compare, logger)
+            amsg = f"{compare} does not exist"
+            self.assertTrue(os.path.exists(compare), msg=amsg)
+            _, compare_root = read_xml_file(compare, logger)
+            diffs = self.xml_diff(xml_root, compare_root)
+            lsep = '\n'
+            amsg = f"{source} does not match {compare}\n{lsep.join(diffs)}"
+            self.assertFalse(diffs, msg=amsg)
+
+#def test_bad_schema_version
+#def test_missing_schema_version
+
+
+
+#        with self.assertRaises(Exception) as context:
+#            # Parse the file
+#            _ = parse_fortran_file(source, self._run_env)
+#        # end if
+#
+#        # Check exception for expected error messages
+#        self.assertTrue("bad_arr1: ';' is not a valid Fortran identifier"
+#                        in str(context.exception))
